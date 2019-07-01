f3c580b011d4e8cdbce13767758c6c0c6e61d18c
John Wen <john.h.wen1@gmail.com>
2019-07-01 14:48:13 -0700

    added inputs

---
+++ calculateSmoothedFiringRateAllCells.m
@@ -0,0 +1,22 @@
+function all_fr = calculateSmoothedFiringRate(matdir, p)
+
+% John Wen 7/1/19
+% Runs Malcolm's firing rate code on all good cells within a .mat file. 
+
+% inputs:
+%     matdir: path to .mat file after running sync_vr_to_np.m
+%     p: params (spatial bin size, etc)
+% outputs:
+%     all_fr: smoothed firing rate over position for each cell
+
+% divide spike counts by occupancy
+
+
+
+%
+
+
+% 
+
+
+end
