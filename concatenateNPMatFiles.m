26598a4ff4bfc19b868a095b80b933c51fc37814
fkmasuda <fkmasuda@gmail.com>
2019-07-22 12:26:34 -0700

    removed requirement that files need licks in order to be stitched in case there is a unity session where the mouse never licked

--- concatenateNPMatFiles.m
+++ concatenateNPMatFiles.m
@@ -2,7 +2,7 @@ function [lickt,lickx,post,posx,trial,sp] = concatenateNPMatFiles(matA, matB)
 % where the data in matA occurs before matB
 
 load(matA,'lickt','lickx','post','posx','sp','trial');
-if isempty(lickt) || isempty(lickx) || isempty(post) || isempty(posx) || isempty(sp) || isempty(trial)
+if isempty(post) || isempty(posx) || isempty(sp) || isempty(trial)
     fprintf(strcat('Coud not find data in: \n', matA, '\n'));
     return
 end
@@ -10,7 +10,7 @@ licktA = lickt; lickxA = lickx; postA = post; posxA=posx; spA = sp; trialA = tri
 
 
 load(matB,'lickt','lickx','post','posx','sp','trial');
-if isempty(lickt) || isempty(lickx) || isempty(post) || isempty(posx) || isempty(sp) || isempty(trial)
+if isempty(post) || isempty(posx) || isempty(sp) || isempty(trial)
     fprintf(strcat('Coud not find data in: \n', matB, '\n'));
     return
 end
