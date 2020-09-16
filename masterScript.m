cd3c4faef2804aabf7322aba876a827d9223ded9
Kei Masuda <keimasuda@Caseys-iMac-Pro.local>
2020-09-16 12:31:57 -0700

    refactored plotting and saving single cell raster plots and then combining them from the allCells struct into its own fxn

--- masterScript.m
+++ masterScript.m
@@ -20,42 +20,28 @@ tic
 sessionMetaDataPath = '/Users/KeiMasuda/Desktop/fkm_analysis/SessionList.xlsx';
 allCells = poolAllCells(filter,sessionMetaDataPath);
 
-% Filter for cells in session where only ketamine was delivered
-seshIndx = ismember(allCells.metadata(:,8),'ketamine');
-ketamineCells = filterAllCellsStruct(allCells,seshIndx);
-fprintf('done filtering ketamineCells\n');
-
-% Filter for only WT mec cells with ketamine
-seshIndx = ismember(ketamineCells.metadata(:,4),'WT');
-fltrCells = filterAllCellsStruct(ketamineCells,seshIndx);
-fprintf('done filtering for WT cells\n');
+% % Filter for cells in session where only ketamine was delivered
+% seshIndx = ismember(allCells.metadata(:,8),'ketamine');
+% ketamineCells = filterAllCellsStruct(allCells,seshIndx);
+% fprintf('done filtering ketamineCells\n');
+% 
+% % Filter for only WT mec cells with ketamine
+% seshIndx = ismember(ketamineCells.metadata(:,4),'WT');
+% fltrCells = filterAllCellsStruct(ketamineCells,seshIndx);
+% fprintf('done filtering for WT cells\n');
 toc
 %% Calculate a decoherence band for each session in given cells
-
-dch = calcDecoherenceBandForSessions(fltrCells);
+dch = calcDecoherenceBandForSessions(allCells);
 save('/Users/keimasuda/Desktop/fkm_analysis/dch.mat','dch')
+%%
+load('/Users/keimasuda/Desktop/fkm_analysis/dch.mat')
 %%
 plotDecoherenceBandStats(dch)
 
 %% Plot Single Cell Raster plots with Decoherence Period Highlighted with Trial-based Dch Index
 save_figs = true;
 image_save_dir = '/Users/KeiMasuda/Desktop/fkm_analysis/rasters_dch';
-for i = 1:numel(seshes)
-    close all;
-    seshIndx = ismember(fltrCells.metadata(:,1),seshes{i});
-    seshCells = filterAllCellsStruct(fltrCells,seshIndx);
-    
-    plotDchRaster(decoherenceIdx,seshCells,i, save_figs,image_save_dir)
-end
-% Combine single cell rasters into large session pngs 
-combinebySesh(fltrCells,image_save_dir)
-%%  Plot Single Cell Raster plots with Decoherence Period Highlighted with Spatially-binned Dch Index
-% save_figs = false;
-% for i = 1:numel(seshes)
-%     seshIndx = ismember(fltrCells.metadata(:,1),seshes{i});
-%     seshCells = filterAllCellsStruct(fltrCells,seshIndx);
-%     plotDchRaster_timeBinnedFR(decoherenceIdx,cells,seshID, Fs, save_figs)
-% end
+plotSaveCombine_SingleCellRastersPlotsWithDecoherence(cells,dch,image_save_dir,save_figs)
 
 %%
-plotAllCells(fltrCells);
+plotAllCells(allCells,dch);
---
+++ plottingFxns/plotSaveCombine_SingleCellRastersPlotsWithDecoherence.m
@@ -0,0 +1,16 @@
+function plotSaveCombine_SingleCellRastersPlotsWithDecoherence(cells,dch,image_save_dir,save_figs)
+% Plot Single Cell Raster plots with Decoherence period 
+% Highlighted with trial-based Dch Index
+% Input: allCells struct, dch struct
+
+for i = 1:numel(seshes)
+    close all;
+    seshIndx = ismember(cells.metadata(:,1),seshes{i});
+    seshCells = filterAllCellsStruct(cells,seshIndx);
+    
+    plotDchRaster(dch.decoherenceIdx,seshCells,i, save_figs,image_save_dir)
+end
+% Combine single cell rasters into large session pngs 
+combinebySesh(fltrCells,image_save_dir)
+
+end
