a04ff36d31b2f5885a9f35fd38c24f345d2a4a01
Kei Masuda <fkmasuda@gmail.com>
2020-10-08 16:02:48 -0700

    modified peakiness curves so that they do not plot the last 10 trials

--- plottingFxns/plot_peakinessCurves.m
+++ plottingFxns/plot_peakinessCurves.m
@@ -1,21 +1,21 @@
-function plot_peakinessCurves(allCells)
+function plot_peakinessCurves(cells)
 addpath(genpath('/Users/KeiMasuda/Documents/MATLAB/Add-Ons/Functions/gramm (complete data visualization toolbox, ggplot2_R-like)/code'));
-metadata = allCells.metadata; %session_name, animalName, sessionDate, genotype, gender, ketamine_day
+metadata = cells.metadata; %session_name, animalName, sessionDate, genotype, gender, ketamine_day
 % Genotype Stairs
 close all;
 figure();
-%%
-genotype = 'WT';
 category = 'peakiness';
+trialMax = 290;
 
 % subplot(3,3,[1,4,7])
 % WTindx = ismember(metadata(:,4),genotype);
-% plot_sortedMatrix(allCells.(category),WTindx,'descend')
+% plot_sortedMatrix(cells.(category),WTindx,'descend')
 % colorbar;
 % title('Peakiness');
 
-subplot(3,3,[1, 2,3]);
-plot_lineWithSEM(allCells.(category),[])
+subplot(3,3,[1,2,3]);
+data = cells.(category);
+plot_lineWithSEM(data(:,1:trialMax),[])
 title('Crest Factor');
 set(gca,'TickDir','out');
 set(gca,'ticklength',[0.005 0.025]);
@@ -26,7 +26,8 @@ set(gca,'FontName','Helvetica');
 
 category = 'bitsPerSpikeCurve';
 subplot(3,3,[4,5,6]);
-plot_lineWithSEM(allCells.(category),[])
+data = cells.(category);
+plot_lineWithSEM(data(:,1:trialMax),[])
 title('Bits Per Spike');
 set(gca,'TickDir','out');
 set(gca,'ticklength',[0.005 0.025]);
@@ -37,7 +38,8 @@ set(gca,'FontName','Helvetica');
 
 category = 'stabilityScoreCurve';
 subplot(3,3,[7,8,9]);
-plot_lineWithSEM(allCells.(category),[])
+data = cells.(category);
+plot_lineWithSEM(data(:,1:trialMax),[])
 title('Stability');
 set(gca,'TickDir','out');
 set(gca,'ticklength',[0.005 0.025]);
